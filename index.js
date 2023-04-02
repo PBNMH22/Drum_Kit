708f0fb8a94c5d5251b4d6bc7c7ceabf14c2e7b5
Deepak Dhage <pbnmh22@gmail.com>
2023-04-02 22:54:15 +0000

    JS updated

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
 function handleClick(){
     
-    var audio = new Audio('/sounds/tom-1.mp3');
-    audio.play();
+    
 }
 }
+
+
+// var audio = new Audio('/sounds/tom-1.mp3');
+//     audio.play();

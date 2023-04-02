e55157c8c372e5bd9665ad9a528a11812e761170
Deepak Dhage <pbnmh22@gmail.com>
2023-04-02 21:27:10 +0000

    added background images for the buttons

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
 
 function handleClick(){
-    alert("I got clicked");
+    
+    var audio = new Audio('/sounds/tom-1.mp3');
+    audio.play();
 }
 }

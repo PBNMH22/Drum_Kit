874f0c34acda7a4c03557ff485b28c89f5161ad5
Deepak Dhage <pbnmh22@gmail.com>
2023-04-05 18:47:45 +0000

    Added cases as per buttons in switch statements

--- index.js
+++ index.js
@@ -17,12 +17,33 @@ switch (buttonInnerHTML){
 
     case "a":
         var audio = new Audio('/sounds/kick-bass.mp3');
-
+        audio.play();
     break;
 
-    
+    case "s":
+        var audio = new Audio('/sounds/snare.mp3');
+        audio.play();
+    break;
 
+    case "d":
+        var audio = new Audio('/sounds/tom-1.mp3');
+        audio.play();
+    break;
 
+    case "j":
+        var audio = new Audio('/sounds/tom-2.mp3');
+        audio.play();
+    break;
+
+    case "k":
+        var audio = new Audio('/sounds/tom-3.mp3');
+        audio.play();
+    break;
+
+    case "l":
+        var audio = new Audio('/sounds/tom-4.mp3');
+        audio.play();
+    break;
 }
 
 }

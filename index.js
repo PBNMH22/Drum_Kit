92a670b1df1a255325705fba5d58f29278574fd0
Deepak Dhage <pbnmh22@gmail.com>
2023-04-05 18:43:22 +0000

    Switch statements added as per the buttons

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ for (let i = 0; i<document.querySelectorAll(".drum").length; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
+var buttonInnerHTML = this.innerHTML;
 
-function handleClick(){
+switch (buttonInnerHTML){
+    case "w":
+        var audio = new Audio('/sounds/crash.mp3');
+        audio.play();
+
+
+    break;
+
+    case "a":
+        var audio = new Audio('/sounds/kick-bass.mp3');
+
+    break;
+
     
-    
-}
-}
 
 
-// var audio = new Audio('/sounds/tom-1.mp3');
-//     audio.play();
+}
+
+}
